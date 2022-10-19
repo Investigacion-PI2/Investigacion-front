343177f55e950ecd1728c1e862c5e1cb83cc9e52
Paola Osorio <61301128+paoos9513@users.noreply.github.com>
2022-10-19 18:21:43 -0500

    Feature/S1/FSOC-38 (#6)
    Co-authored-by: Bryan A. Guapacha F <48836505+BryanGF0822@users.noreply.github.com>

---
+++ src/layout/auth/Login.tsx
@@ -0,0 +1,206 @@
+import React from 'react'
+import { useToggle, upperFirst } from '@mantine/hooks';
+import { useForm } from '@mantine/form';
+import {
+    TextInput,
+    PasswordInput,
+    Text,
+    Paper,
+    Group,
+    PaperProps,
+    Button,
+    Divider,
+    Checkbox,
+    Anchor,
+    Stack,
+    Container,
+    MantineProvider,
+    createStyles,  Title, Overlay
+  } from '@mantine/core';
+
+  const useStyles = createStyles((theme) => ({
+    hero: {
+      position: 'relative',
+      backgroundImage: 'url(/public/saman.jpeg)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
+  
+    container: {
+      height: 700,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'flex-start',
+      paddingBottom: theme.spacing.xl * 6,
+      zIndex: 1,
+      position: 'relative',
+  
+      [theme.fn.smallerThan('sm')]: {
+        height: 500,
+        paddingBottom: theme.spacing.xl * 3,
+      },
+    },
+    containerImage:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 50
+    },
+    image:{
+        width: 350,
+        height:150,
+        
+    },
+    center:{
+      alignItems:'center',
+      justifyContent:'center',
+      textAlign: 'center',
+      fontWeight:'bold'
+    },
+
+  
+    blackBox: {
+      position: 'absolute',
+      borderRadius: '6px',
+      backdropFilter: 'blur(3px)',
+      backgroundImage: "linear-gradient(180deg, rgba(0, 0, 0, 0.25) 99.99%, rgba(217, 217, 217, 0) 100%)",
+    },
+
+  
+    title: {
+      color: theme.white,
+      fontSize: 60,
+      fontWeight: 900,
+      lineHeight: 1.1,
+      zIndex: 1,
+  
+      [theme.fn.smallerThan('sm')]: {
+        fontSize: 40,
+        lineHeight: 1.2,
+      },
+  
+      [theme.fn.smallerThan('xs')]: {
+        fontSize: 28,
+        lineHeight: 1.3,
+      },
+    },
+  
+    description: {
+      color: theme.white,
+      maxWidth: 600,
+  
+      [theme.fn.smallerThan('sm')]: {
+        maxWidth: '100%',
+        fontSize: theme.fontSizes.sm,
+      },
+    },
+    button:{
+      alignItems:'center',
+      justifyContent:'center',
+    },
+    control: {
+      marginTop: theme.spacing.xl * 1.5,
+      [theme.fn.smallerThan('sm')]: {
+        width: '100%',
+      },
+    },
+  }));
+
+export function Login (props: PaperProps) {
+    const form = useForm({
+      initialValues: {
+        user: '',
+        password: '',
+        terms: true,
+      },
+  
+      validate: {
+        user: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid user'),
+        password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
+      },
+    });
+    const { classes } = useStyles();
+
+  
+  return (
+    <div className={classes.hero}>
+        <div className={classes.container}>
+    <MantineProvider
+      theme={{
+        components: {
+          Container: {
+            defaultProps: {
+              sizes: {
+                xs: 540,
+                sm: 720,
+                md: 960,
+                lg: 1140,
+                xl: 1320,
+              },
+            },
+          },
+        },
+      }}
+    >
+        
+            <Container size="xs">      
+            <Paper radius="md" p="xl" withBorder {...props}>
+                <div className={classes.containerImage}>
+            <img src="/public/icesi-logo2.png" className={classes.image}/>
+            </div>
+           
+           <div className={classes.center}>
+            <Text size="lg" weight={500}>
+                Grupos de investigación
+            </Text>
+            </div>
+
+            <form onSubmit={form.onSubmit(() => {})}>
+                <Stack>
+
+                <TextInput
+                    required
+                    label="Usuario:"
+                    placeholder="Usuario"
+                    value={form.values.user}
+                    onChange={(event) => form.setFieldValue('user', event.currentTarget.value)}
+                    error={form.errors.user && 'Usuario inválido'}
+                />
+
+                <PasswordInput
+                    required
+                    label="Contraseña:"
+                    placeholder="Contraseña"
+                    value={form.values.password}
+                    onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+                    error={form.errors.password && 'Contraseña inválida'}
+                />
+
+                </Stack>
+
+                <Group position="apart" mt="xl">
+                <Anchor
+                    component="button"
+                    type="button"
+                    color="dimmed"
+                    onClick={() =>{}}
+                    size="xs"
+                >
+                    
+                </Anchor>
+                <div className={classes.button}>
+                <Button type="submit" >Iniciar sesión</Button>
+                </div>
+                </Group>
+            </form>
+            </Paper>
+            </Container>
+    
+    </MantineProvider>
+    
+    </div>
+    </div>
+  );
+}
+
+  
